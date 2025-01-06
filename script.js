83bebdf0c97cc6e8c9d291f1ed2413d588d619f7
Passionseekerr <passionseekerr@gmail.com>
2025-01-06 19:31:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+const backToTop = document.querySelector('.back-to-top');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) {
+    backToTop.classList.add('visible');
+  } else {
+    backToTop.classList.remove('visible');
+  }
+});
+
+backToTop.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
